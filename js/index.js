5814042705b3870426c5dd3b05587c8d4415c887
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-22 22:00:05 +0300

    getMovies function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 const baseURL="http://localhost:3000/films"
+getMovies()
 function getMovies(){
     fetch(baseURL)
     .then((response) => response.json())
-    .then(data => data )
+    .then(data => {
+        
+    });    
 }
+getMovies()
+function displayMoviesShowing(films){
+    const movieListing= document.getElementById("movieListingArea");
+    for(const film of films) {
+        console.log(film)
+        let movies = document.getElementById("name")
+        let li = document.createElement('li')
+        li.textContent= film.name 
+        li.addEventListener('click', () => getFilmInfo(film));
+        movies.appendChild(li)
+    }
+}
+
